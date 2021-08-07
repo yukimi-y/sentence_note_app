cfbf16037fabc3b0b689db442ef6f7ce64fbdcb7
yukimi.y <yyy.yukimi@gmail.com>
2021-08-07 19:27:32 +0900

    bootstrapのjavascript修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery'
-}))
-
 module.exports = environment
+
+  const webpack = require('webpack')  
+  environment.plugins.append(
+    'Provide',    
+    new webpack.ProvidePlugin({      
+        $: 'jquery/src/jquery',      
+        jQuery: 'jquery/src/jquery',      
+        Popper: ['popper.js', 'default']    
+    })  
+  )
